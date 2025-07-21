c0960adbae666c98b633f254df9001c775786020
Travis Jenkins <travissjenkins@icloud.com>
2025-07-21 16:52:14 -0400

    made new variables to generate new player and computer selections for each round and score tally function that stops the game if computer or player score reaches 3 (best 3 out of 5)

--- script.js
+++ script.js
@@ -42,6 +42,11 @@ let humanScore = 0;
 let computerScore = 0;
 
 
+
+/**human choice is tested against computer choice in the round 
+ * I need the restart the function in case of a tie..
+ * but not sure how ATM
+*/
 function playRound(humanChoice,computerChoice) {
     console.log(computerChoice);
     console.log(humanChoice);
@@ -56,15 +61,79 @@ function playRound(humanChoice,computerChoice) {
     } else {
         return "computer won :(" && computerScore ++;
     }
-    }
+}
+
+
+
+
+/**setting up 5 variables with different names 
+ * so they get called again with new values */
 
 const humanSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
+const humanSelection2 = getPlayerChoice();
+const computerSelection2 = getComputerChoice();
+const humanSelection3 = getPlayerChoice();
+const computerSelection3 = getComputerChoice();
+const humanSelection4 = getPlayerChoice();
+const computerSelection4 = getComputerChoice();
+const humanSelection5 = getPlayerChoice();
+const computerSelection5 = getComputerChoice();
+const humanSelection6 = getPlayerChoice();
+const computerSelection6 = getComputerChoice();
+const humanSelection7 = getPlayerChoice();
+const computerSelection7 = getComputerChoice();
+const humanSelection8 = getPlayerChoice();
+const computerSelection8 = getComputerChoice();
+const humanSelection9 = getPlayerChoice();
+const computerSelection9 = getComputerChoice();
+const humanSelection10 = getPlayerChoice();
+const computerSelection10 = getComputerChoice();
+/**calling the rounds. Trouble is with instances of a tie..
+ * I could safely just make say 10 variable and 
+ * in all likely hood, there wouldn't be instances of 5 ties. 
+ */
 
 console.log(playRound(humanSelection,computerSelection));
 console.log("player score: " + humanScore);
 console.log("computer score is " + computerScore);
+console.log(playRound(humanSelection2,computerSelection2));
+console.log("player score: " + humanScore);
+console.log("computer score is " + computerScore);
+console.log(playRound(humanSelection3,computerSelection3));
+console.log("player score: " + humanScore);
+console.log("computer score is " + computerScore);
+console.log(playRound(humanSelection4,computerSelection4));
+console.log("player score: " + humanScore);
+console.log("computer score is " + computerScore);
+console.log(playRound(humanSelection5,computerSelection5));
+console.log("player score: " + humanScore);
+console.log("computer score is " + computerScore);
+console.log(playRound(humanSelection6,computerSelection6));
+console.log("player score: " + humanScore);
+console.log("computer score is " + computerScore);
+console.log(playRound(humanSelection7,computerSelection7));
+console.log("player score: " + humanScore);
+console.log("computer score is " + computerScore);
+console.log(playRound(humanSelection8,computerSelection8));
+console.log("player score: " + humanScore);
+console.log("computer score is " + computerScore);
+console.log(playRound(humanSelection9,computerSelection9));
+console.log("player score: " + humanScore);
+console.log("computer score is " + computerScore);
+console.log(playRound(humanSelection10,computerSelection10));
+console.log("player score: " + humanScore);
+console.log("computer score is " + computerScore);
+
+/**getting the tally of best three out of 5.  */
+if (humanScore == 3) {
+      return console.log("Player won!"); 
+    } else if (computerScore == 3) {
+        return console.log("computer won");
+    }
 }
+
+
 
 
 
